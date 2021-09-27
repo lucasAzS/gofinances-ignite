7dc2d13dd13ad9b6630fcda8211adae6bcce5cdc
Lucas de Azambuja dos Santos <lucasazsantos@gmail.com>
2021-09-26 21:26:38 -0300

    global style and theme provider of styled components

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    primary: '#5636D3',
+
+    secondary: '#FF872C',
+
+    secondary_light: '#FF9C4A',
+
+    success: '#12A454',
+    success_light: 'rgba(18, 164, 84, 0.5)',
+
+    attention: '#E83f5b',
+    attention_light: 'rgba(232, 63, 91, 0.5)',
+
+    shape: '#E5E5E5',
+    title: '#363f5f',
+    text: '#969cb2',
+    background: '#f0f2f5',
+  },
+};
