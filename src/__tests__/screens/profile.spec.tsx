c483c0b078e364421e7c1c6432893cbbb1a4840c
Lucas de Azambuja dos Santos <lucasazsantos@gmail.com>
2022-04-06 17:55:48 -0300

    Seletores

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -10,3 +10,22 @@ test('check if shows correct input name placeholder ', () => {
 
   expect(inputName).toBeTruthy();
 });
+
+test('check if user data has been loaded', () => {
+  const { getByTestId } = render(<Profile />);
+
+  const inputName = getByTestId('input-name');
+  const inputSurname = getByTestId('input-surname');
+
+  expect(inputName.props.value).toEqual('Lucas');
+  expect(inputSurname.props.value).toEqual('de Azambuja');
+});
+
+test('check if title renders correctly', () => {
+  const { getByTestId } = render(<Profile />);
+
+  const title = getByTestId('text-title');
+
+  expect(title).toBeTruthy();
+  expect(title.props.children).toContain('Perfil');
+});
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -4,11 +4,20 @@ import { View, Text, TextInput, Button } from 'react-native';
 export function Profile() {
   return (
     <View>
-      <Text>Perfil</Text>
+      <Text testID='text-title'>Perfil</Text>
 
-      <TextInput placeholder='Nome' autoCorrect={false} />
+      <TextInput
+        testID='input-name'
+        placeholder='Nome'
+        autoCorrect={false}
+        value='Lucas'
+      />
 
-      <TextInput placeholder='Sobrenome' />
+      <TextInput
+        testID='input-surname'
+        placeholder='Sobrenome'
+        value='de Azambuja'
+      />
 
       <Button title='Salvar' onPress={() => {}} />
     </View>
