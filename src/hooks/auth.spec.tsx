e72ebecfd63b640476ec3ffc7355870ff154230c
Lucas de Azambuja dos Santos <lucasazsantos@gmail.com>
2022-04-07 15:28:11 -0300

    Mocks Personalizados

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,29 +1,44 @@
 import { renderHook, act } from '@testing-library/react-hooks';
+import { mocked } from 'ts-jest/utils';
+import { startAsync } from 'expo-auth-session';
+
 import { AuthProvider, useAuth } from './auth';
 
-jest.mock('expo-auth-session', () => {
-  return {
-    startAsync: () => {
-      return {
-        type: 'success',
-        user: {
-          id: 'any email',
-          email: 'lucas@email.com',
-          name: 'Lucas',
-          photo: 'any_photo.png',
-        },
-      };
-    },
-  };
-});
+jest.mock('expo-auth-session');
 
 describe('Auth Hook', () => {
   it('should be able to login with Google account', async () => {
+    const googleMocked = mocked(startAsync as any);
+    googleMocked.mockReturnValueOnce({
+      type: 'success',
+      user: {
+        id: 'any_id',
+        email: 'lucas@email.com',
+        name: 'Lucas',
+        photo: 'any_photo.png',
+      },
+    });
+
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
 
     await act(() => result.current.signInWithGoogle());
     expect(result.current.user).toBeTruthy();
+  });
+
+  it('user should not connect if cancel auth with google', async () => {
+    const googleMocked = mocked(startAsync as any);
+    googleMocked.mockReturnValueOnce({
+      type: 'cancel',
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    await act(() => result.current.signInWithGoogle());
+
+    expect(result.current.user).not.toHaveProperty('id');
   });
 });
